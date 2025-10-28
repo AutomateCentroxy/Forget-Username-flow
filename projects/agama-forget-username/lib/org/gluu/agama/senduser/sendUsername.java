cc96554aa0fc1737b3f9fbe4ae24119bd4068664
AutomateCentroxy <155064161+AutomateCentroxy@users.noreply.github.com>
2025-10-28 19:37:32 +0530

    refactor: update sendUsername.java

--- projects/agama-forget-username/lib/org/gluu/agama/senduser/sendUsername.java
+++ projects/agama-forget-username/lib/org/gluu/agama/senduser/sendUsername.java
@@ -5,10 +5,19 @@ import org.gluu.agama.forgetusername.jansForgetUsername;
 
 public abstract class forgetusername {
 
+    /**
+     * Fetch user details by email.
+     */
     public abstract Map<String, String> getUserEntityByMail(String mail);
 
+    /**
+     * Send username email to the user.
+     */
     public abstract boolean sendUsernameEmail(String to, String userName, String lang);
 
+    /**
+     * Factory method to return an instance of jansForgetUsername.
+     */
     public static jansForgetUsername getInstance() {
         return new jansForgetUsername();
     }
