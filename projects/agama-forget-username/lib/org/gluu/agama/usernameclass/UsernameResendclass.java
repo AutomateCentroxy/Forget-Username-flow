0eb0906442be32934f4fc86549e6d6a58b5178a6
AutomateCentroxy <155064161+AutomateCentroxy@users.noreply.github.com>
2025-10-28 22:40:14 +0530

    feat: add UsernameResendclass.java

---
+++ projects/agama-forget-username/lib/org/gluu/agama/usernameclass/UsernameResendclass.java
@@ -0,0 +1,15 @@
+package org.gluu.agama.usernameclass;
+
+import java.util.Map;
+import org.gluu.agama.forgetusername.jansForgetUsername;
+
+public abstract class UsernameResendclass {
+
+    public abstract Map<String, String> getUserEntityByMail(String email);
+
+    public abstract boolean sendUsernameEmail(String to, String usernam, String lang);
+
+    public static UsernameResendclass getInstance() {
+        return new jansForgetUsername();
+    }
+}
