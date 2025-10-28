4059ef5cf7887291de2b4156015220302c482307
AutomateCentroxy <155064161+AutomateCentroxy@users.noreply.github.com>
2025-10-28 22:44:26 +0530

    refactor: update UsernameResendclass.java

--- projects/agama-forget-username/lib/org/gluu/agama/usernameclass/UsernameResendclass.java
+++ projects/agama-forget-username/lib/org/gluu/agama/usernameclass/UsernameResendclass.java
@@ -7,9 +7,9 @@ public abstract class UsernameResendclass {
 
     public abstract Map<String, String> getUserEntityByMail(String email);
 
-    public abstract boolean sendUsernameEmail(String to, String usernam, String lang);
+    public abstract boolean sendUsernameEmail(String to, String username, String lang);
 
     public static UsernameResendclass getInstance() {
-        return new ansForgetUsername();
+        return new JansForgetUsername();
     }
 }
